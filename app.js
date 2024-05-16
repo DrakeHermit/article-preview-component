1ccd24ee4ecd8dbe8341b6f89916faf85e629e0f
Jan Kotvaš <jan.kotvas96@gmail.com>
2024-05-16 19:41:10 +0200

    Finished the desktop version of the desing and added some popup functionality for the button

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const shareBtn = document.getElementById("share");
 const shareBtns = document.getElementById("shareBtns");
 const shareBtnsActive = document.getElementById("shareActive");
+const shareBtnsDesktop = document.getElementById("shareBtnsDesktop");
+
+if (window.innerWidth > 1000) {
+  shareBtnsDesktop.style.display = "none";
+  console.log(window.innerWidth);
+}
 
 shareBtn.addEventListener("click", (e) => {
   shareBtns.style.zIndex = 1;
   shareBtns.style.opacity = 1;
+  shareBtnsDesktop.style.display = "block";
   console.log("sss");
 });
 
 shareBtnsActive.addEventListener("click", () => {
   shareBtns.style.zIndex = -1;
   shareBtns.style.opacity = 1;
+  shareBtnsDesktop.style.display = "none";
 });
