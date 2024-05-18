4da6aac00ee0196edc7efba03da782042588101c
Jan Kotvaš <jan.kotvas96@gmail.com>
2024-05-18 20:32:55 +0200

    Finished challenge

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 const shareBtn = document.getElementById("share");
-const shareBtns = document.getElementById("shareBtns");
+const btnList = document.getElementById("shareBtns");
 const shareBtnsActive = document.getElementById("shareActive");
 const shareBtnsDesktop = document.getElementById("shareBtnsDesktop");
+const svgArrow = document.getElementById("arrow");
 
-if (window.innerWidth > 1000) {
+const viewportWidth = window.innerWidth;
+
+if (viewportWidth < 600) {
   shareBtnsDesktop.style.display = "none";
-  console.log(window.innerWidth);
+
+  shareBtn.addEventListener("click", () => {
+    btnList.style.zIndex = 1;
+  });
+
+  shareBtnsActive.addEventListener("click", () => {
+    btnList.style.zIndex = -1;
+  });
 }
 
-shareBtn.addEventListener("click", (e) => {
-  shareBtns.style.zIndex = 1;
-  shareBtns.style.opacity = 1;
-  shareBtnsDesktop.style.display = "block";
-  console.log("sss");
-});
-
-shareBtnsActive.addEventListener("click", () => {
-  shareBtns.style.zIndex = -1;
-  shareBtns.style.opacity = 1;
+if (viewportWidth > 1000) {
   shareBtnsDesktop.style.display = "none";
-});
+  let clicked = 0;
+  shareBtn.addEventListener("click", () => {
+    clicked++;
+    shareBtnsDesktop.style.display = "block";
+    shareBtn.style.backgroundColor = "#6E8098";
+    svgArrow.setAttribute("fill", "white");
+    if (clicked % 2 === 0) {
+      shareBtnsDesktop.style.display = "none";
+      shareBtn.style.backgroundColor = "#ECF2F8";
+      svgArrow.setAttribute("fill", "#6E8098");
+    }
+  });
+}
